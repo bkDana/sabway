a210c126e94f370363f8fde35bfdc6375271d8ca
wjddja003 <003645@naver.com>
2019-06-28 11:32:29 +0900

    no message

--- src/main/java/kr/co/subway/order/controller/OrderController.java
+++ src/main/java/kr/co/subway/order/controller/OrderController.java
@@ -1,4 +1,4 @@
-package kr.co.subway.order;
+package kr.co.subway.order.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
