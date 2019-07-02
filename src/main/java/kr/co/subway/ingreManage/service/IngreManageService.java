149cdfadac8632cc05080d3d83e99ba5856cf399
bk_dana <qhrud7915@gmail.com>
2019-07-02 20:30:01 +0900

    재료 리스트

--- src/main/java/kr/co/subway/ingreManage/service/IngreManageService.java
+++ src/main/java/kr/co/subway/ingreManage/service/IngreManageService.java
@@ -1,10 +1,15 @@
 package kr.co.subway.ingreManage.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.subway.ingreManage.dao.IngreManageDao;
+import kr.co.subway.ingreManage.vo.IngrePageNaviData;
 import kr.co.subway.ingreManage.vo.IngreVo;
+import kr.co.subway.notice.vo.PageBound;
 
 @Service("menuManageService")
 public class IngreManageService {
@@ -15,5 +20,42 @@ public class IngreManageService {
 	public int ingreReg(IngreVo iv) {
 		int result = ingreDao.ingreReg(iv);
 		return result;
+	}
+	
+	//재료 리스트 가져오기
+	public IngrePageNaviData ingreList(int reqPage, String searchType, String searchVal) {
+		int numPerPage = 10;
+		int totalCount = ingreDao.ingreTotalCount(searchType, searchVal);
+		int totalPage = (totalCount%numPerPage==0)?(totalCount/numPerPage):(totalCount/numPerPage)+1;
+		int start = (reqPage-1)*numPerPage+1;
+		int end = reqPage*numPerPage;
+		PageBound pb = new PageBound(start, end);
+		ArrayList<IngreVo> ingreList = (ArrayList<IngreVo>)ingreDao.ingreSelectAll(pb,searchType, searchVal);
+		String pageNavi = "";
+		int pageNaviSize = 5;
+		int pageNo = ((reqPage-1)/pageNaviSize)*pageNaviSize+1;
+		if(reqPage != 1) {
+			pageNavi += "<a class='paging-arrow prev-arrow' href='/ingreList.do?reqPage="+(pageNo-1)+"&searchType="+searchType+"&searchVal="+searchVal+"'>"+pageNo+"'>이전</a>";
+		}
+		int i = 1;
+		while(!(i++>pageNaviSize || pageNo>totalPage)) {
+			if(reqPage == pageNo) {
+				pageNavi += "<span class='cur'>"+pageNo+"</span>";
+			}else {
+				pageNavi += "<a href='/ingreList.do?reqPage="+pageNo+"&searchType="+searchType+"&searchVal="+searchVal+"'>"+pageNo+"</a>";
+			}
+			pageNo++;
+		}
+		if(reqPage < totalPage) {
+			pageNavi +="<a class='paging-arrow next-arrow' href='/ingreList.do?reqPage="+pageNo+"&searchType="+searchType+"&searchVal="+searchVal+"'>"+pageNo+"'>다음</a>";
+		}
+		
+		return new IngrePageNaviData(ingreList, pageNavi);
+	}
+	
+	//재료 리스트 페이지에서 검색박스에서 재료 카테로기 선택시 하위 값 가져오기
+	public List ingreType() {
+		List list = ingreDao.ingreType();
+		return list;
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/admin/ingreManage/ingreUpdate.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%-- Header --%>
+<jsp:include page="/WEB-INF/views/admin/common/header.jsp" />
+
+
+<%-- Content --%>
+<section id="content-wrapper" class="clearfix">
+	<jsp:include page="/WEB-INF/views/admin/common/admin-left-nav.jsp" />
+	<div class="area">
+		<div class="sub-menu">※ 메뉴관리 > 메뉴수정</div>
+
+
+	</div>
+</section>
+
+
+
+<%-- Footer --%>
+<jsp:include page="/WEB-INF/views/admin/common/footer.jsp" />
