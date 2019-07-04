c90b5e548820cc5617b8fb2bbe345cd4ab45d0d0
wjddja003 <003645@naver.com>
2019-07-04 18:11:44 +0900

    2019-0704 editor 완료!

--- src/main/webapp/resources/js/editor.js
+++ src/main/webapp/resources/js/editor.js
@@ -1,8 +1,30 @@
 //첨부파일 추가
-	var rowIndex = 0;
+var rowIndex = 0;
+$(document).ready(function(){
+	$('#testContent').keyup(function(event) {
+		if ( event.keyCode === 8 || event.keyCode === 46) {
+	    	for(var i=0;i<rowIndex; i++){
+	    		if($("#testContent").html().indexOf("fileDiv"+i) == -1) {
+	    			$("#fileName"+i).parent().remove();
+	    		}
+	    	}
+		}
+
+	});
+});
+
+	$(document).on("dragstart","#testContent",function(e){
+	    console.log("d"+e);
+	    return false;
+	});
+	$(document).on("click",".resize-div",function(){		
+		$(this).css("resize", "both");
+	});
 
 	function addFile(form){
-	
+		if($("#fileName"+rowIndex).val()==""){
+			return;
+		}
 	    rowIndex++;
 	    var getTable = document.getElementById("insertTable");
 		var oCurrentRow = getTable.insertRow(getTable.rows.length);
@@ -36,7 +58,10 @@
 				"<input type='file' class='editor-file-upload' id='editor-file-upload"+rowIndex+"' name='filename"+ rowIndex + "' size=25 " +
 				"onchange='onChangeImg(this,"+rowIndex+")'"
 				+"></td>" +
-				"<td width=100><input type='button' value='삭제' onClick='deleteFile(this,"+rowIndex+")' style='cursor:hand'></td></tr>";
+				"<td width=100><input type='button' value='삭제' onClick='deleteFile(this,"+rowIndex+")' style='cursor:pointer'></td></tr>";
+		}else{
+			$("#fileName"+Index).val(f.value);
+			$("#fileDiv"+Index).remove();
 		}
 	    
 		if(f.files.length!=0 && f.files[0]!=0){ //f.file -> 선택한 파일을 가져옴 (배열형태로) , f.files[0] -> 0번재 파일의 크기
@@ -46,23 +71,17 @@
 			reader.readAsDataURL(f.files[0]);	//선택한 파일의 경로를 읽어옴
 			console.log(f.files[0]);
 			reader.onload = function(e){
+				
 				innerContent += "<div id='fileDiv" + Index + "' class='resize-div' contentEditable='false'>"
-					+"<img class='resize-img' width='100%' height='100%' src='"+e.target.result+"'></div><br>";
+				+"<img class='resize-img' width='100%' height='100%' src='"+e.target.result+"'></div>\r\n";
 				$("#testContent").html(innerContent);
 			}
 		} else{ //파일을 뺄 경우
 			$("#fileName"+Index).val(f.value);
-			console.log($('.resize-div'));
 			$("#fileName"+Index).parent().remove();
 			$("#fileDiv"+Index).remove();
 		}
 	}
 	
-	$(document).keydown(function(e){
-		console.log("???");
-		if(e.keyCode == 8){
-			var leng = $('.resize-div').length;
-			console.log(leng);
-		}
-	});
-
+	
+	
