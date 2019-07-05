f77a68445fdd6bbed1c8a5ea6d8bd99c9f34c6a2
조성일 <roseshade90@gmail.com>
2019-07-05 19:15:11 +0900

    프로모션 적용(임시 menu 테이블 및 vo 생성함), rowmapper삭제,주석추가,

--- src/main/java/kr/co/subway/headOffice/dao/ApplyDAO.java
+++ src/main/java/kr/co/subway/headOffice/dao/ApplyDAO.java
@@ -12,16 +12,19 @@ import kr.co.subway.headOffice.vo.Apply;
 public class ApplyDAO {
 	@Autowired
 	SqlSession sqlsession;
-	
+	//신청목록
 	public List applyList() {
 		return sqlsession.selectList("apply.applyList");
 	}
+	//승인처리
 	public int applyManagerUpdate(String applyName) {
 		return sqlsession.update("apply.applyManagerUpdate",applyName);
 	}
+	//거절처리
 	public int rejectManagerUpdate(String applyName) {
 		return sqlsession.update("apply.rejectManagerUpdate",applyName);
 	}
+	//신청 글 상세보기
 	public Apply applyView(int applyNo) {
 		return sqlsession.selectOne("apply.applyView",applyNo);
 	}
--- src/main/java/kr/co/subway/manager/service/MgrService.java
+++ src/main/java/kr/co/subway/manager/service/MgrService.java
@@ -1,8 +1,5 @@
 package kr.co.subway.manager.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -19,6 +16,7 @@ public class MgrService {
 	@Resource(name="applyDao")
 	ApplyDAO applydao;
 
+	//승인한 가맹점 등록
 	@Transactional
 	public int enrollMgr(Mgr mg,String applyName) {
 		int result = mgrdao.enrollMgr(mg);
@@ -29,9 +27,9 @@ public class MgrService {
 //		List list= mgrdao.selectMgr();
 //		return (ArrayList<Mgr>)list;
 //	}
-	
+	//관리자 로그인
 	public Mgr login(String mgrId) {
-	Mgr mgr = mgrdao.login(mgrId);
-	return mgr;
-}
+		Mgr mgr = mgrdao.login(mgrId);
+		return mgr;
+	}
 }
