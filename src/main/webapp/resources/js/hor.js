c12b25b969210e0427275ae6b055e2d7cb302557
emthrology <emthrology@gmail.com>
2019-07-14 08:49:43 +0900

    Update hor.js

--- src/main/webapp/resources/js/hor.js
+++ src/main/webapp/resources/js/hor.js
@@ -192,107 +192,107 @@ $('input[name=sideRef]:gt(0)').change(function(){
 
 /* 샌드위치/샐러드 주재료 태그문자열 */
 tagSandwich = '<h3>메인재료</h3> \
-	<label for="이탈리안비엠티"><img alt="이탈리안비엠티" src="/resources/img/sandwich/sandwich_cl01.jpg"></label> \
-	<input type="radio" class="hide" id="이탈리안비엠티" name="main" value="0"> \
-	<label for="비엘티"><img alt="비엘티" src="/resources/img/sandwich/sandwich_cl02.jpg"></label> \
-	<input type="radio" class="hide" id="비엘티" name="main" value="1"> \
-	<label for="미트볼"><img alt="미트볼" src="/resources/img/sandwich/sandwich_cl03.jpg"></label> \
-	<input type="radio" class="hide" id="미트볼" name="main" value="2"> \
-	<label for="햄"><img alt="햄" src="/resources/img/sandwich/sandwich_cl04.jpg"></label> \
-	<input type="radio" class="hide" id="햄" name="main" value="3"> \
-	<label for="참치"><img alt="참치" src="/resources/img/sandwich/sandwich_cl05.jpg"></label> \
-	<input type="radio" class="hide" id="참치" name="main" value="4"> \
-	<label for="에그마요"><img alt="에그마요" src="/resources/img/sandwich/sandwich_cl06.jpg"></label> \
-	<input type="radio" class="hide" id="에그마요" name="main" value="5"> \
-	<label for="로티세리치킨"><img alt="로티세리" src="/resources/img/sandwich/sandwich_fl01.jpg"></label> \
-	<input type="radio" class="hide" id="로티세리치킨" name="main" value="6"> \
-	<label for="로스트치킨"><img alt="로스트치킨" src="/resources/img/sandwich/sandwich_fl02.jpg"></label> \
-	<input type="radio" class="hide" id="로스트치킨" name="main" value="7"> \
-	<label for="로스트비프"><img alt="로스트비프" src="/resources/img/sandwich/sandwich_fl03.jpg"></label> \
-	<input type="radio" class="hide" id="로스트비프" name="main" value="8"> \
-	<label for="사브웨이클럽"><img alt="사브웨이클럽" src="/resources/img/sandwich/sandwich_fl04.jpg"></label> \
-	<input type="radio" class="hide" id="사브웨이클럽" name="main" value="9"> \
-	<label for="터키"><img alt="터키" src="/resources/img/sandwich/sandwich_fl05.jpg"></label> \
-	<input type="radio" class="hide" id="터키" name="main" value="10"> \
-	<label for="베지"><img alt="베지" src="/resources/img/sandwich/sandwich_fl06.jpg"></label> \
-	<input type="radio" class="hide" id="베지" name="main" value="11"> \
-	<label for="스테이크&치즈"><img alt="스테이크&치즈" src="/resources/img/sandwich/sandwich_pm01.jpg"></label> \
-	<input type="radio" class="hide" id="스테이크&치즈" name="main" value="12"> \
-	<label for="터키베이컨아보카도"><img alt="터키베이컨아보카도" src="/resources/img/sandwich/sandwich_pm02.jpg"></label> \
-	<input type="radio" class="hide" id="터키베이컨아보카도" name="main" value="13"> \
-	<label for="치킨베이컨랜치"><img alt="치킨베이컨랜치" src="/resources/img/sandwich/sandwich_pm03.jpg"></label> \
-	<input type="radio" class="hide" id="치킨베이컨랜치" name="main" value="14"> \
-	<label for="사브웨이멜트"><img alt="사브웨이멜트" src="/resources/img/sandwich/sandwich_pm04.jpg"></label> \
-	<input type="radio" class="hide" id="사브웨이멜트" name="main" value="15"> \
-	<label for="터키베이컨"><img alt="터키베이컨" src="/resources/img/sandwich/sandwich_pm05.jpg"></label> \
-	<input type="radio" class="hide" id="터키베이컨" name="main" value="16"> \
-	<label for="스파이시이탈리안"><img alt="스파이시이탈리안" src="/resources/img/sandwich/sandwich_pm06.jpg"></label> \
-	<input type="radio" class="hide" id="스파이시이탈리안" name="main" value="17"> \
-	<label for="치킨데리야끼"><img alt="치킨데리야끼" src="/resources/img/sandwich/sandwich_pm07.jpg"></label> \
-	<input type="radio" class="hide" id="치킨데리야끼" name="main" value="18"> \
-	<label for="풀드포크"><img alt="풀드포크" src="/resources/img/sandwich/sandwich_pm08.jpg"></label> \
-	<input type="radio" class="hide" id="풀드포크" name="main" value="19"> \
 	<label for="쉬림프"><img alt="쉬림프" src="/resources/img/sandwich/sandwich_pm10.jpg"></label> \
-	<input type="radio" class="hide" id="쉬림프" name="main" value="20"> \
+	<input type="radio" class="hide" id="쉬림프" name="main" value="1"> \
 	<label for="쉬림프아보카도"><img alt="쉬림프아보카도" src="/resources/img/sandwich/sandwich_pm11.jpg"></label> \
-	<input type="radio" class="hide" id="쉬림프아보카도" name="main" value="21"> \
+	<input type="radio" class="hide" id="쉬림프아보카도" name="main" value="2"> \
+	<label for="풀드포크"><img alt="풀드포크" src="/resources/img/sandwich/sandwich_pm08.jpg"></label> \
+	<input type="radio" class="hide" id="풀드포크" name="main" value="3"> \
+	<label for="스테이크&치즈"><img alt="스테이크&치즈" src="/resources/img/sandwich/sandwich_pm01.jpg"></label> \
+	<input type="radio" class="hide" id="스테이크&치즈" name="main" value="4"> \
+	<label for="터키베이컨아보카도"><img alt="터키베이컨아보카도" src="/resources/img/sandwich/sandwich_pm02.jpg"></label> \
+	<input type="radio" class="hide" id="터키베이컨아보카도" name="main" value="5"> \
+	<label for="치킨베이컨랜치"><img alt="치킨베이컨랜치" src="/resources/img/sandwich/sandwich_pm03.jpg"></label> \
+	<input type="radio" class="hide" id="치킨베이컨랜치" name="main" value="6"> \
+	<label for="터키베이컨"><img alt="터키베이컨" src="/resources/img/sandwich/sandwich_pm05.jpg"></label> \
+	<input type="radio" class="hide" id="터키베이컨" name="main" value="7"> \
+	<label for="스파이시이탈리안"><img alt="스파이시이탈리안" src="/resources/img/sandwich/sandwich_pm06.jpg"></label> \
+	<input type="radio" class="hide" id="스파이시이탈리안" name="main" value="8"> \
+	<label for="치킨데리야끼"><img alt="치킨데리야끼" src="/resources/img/sandwich/sandwich_pm07.jpg"></label> \
+	<input type="radio" class="hide" id="치킨데리야끼" name="main" value="9"> \
+	<label for="로티세리치킨"><img alt="로티세리" src="/resources/img/sandwich/sandwich_fl01.jpg"></label> \
+	<input type="radio" class="hide" id="로티세리치킨" name="main" value="10"> \
+	<label for="로스트비프"><img alt="로스트비프" src="/resources/img/sandwich/sandwich_fl03.jpg"></label> \
+	<input type="radio" class="hide" id="로스트비프" name="main" value="11"> \
+	<label for="사브웨이클럽"><img alt="사브웨이클럽" src="/resources/img/sandwich/sandwich_fl04.jpg"></label> \
+	<input type="radio" class="hide" id="사브웨이클럽" name="main" value="12"> \
+	<label for="베지"><img alt="베지" src="/resources/img/sandwich/sandwich_fl06.jpg"></label> \
+	<input type="radio" class="hide" id="베지" name="main" value="13"> \
+	<label for="에그마요"><img alt="에그마요" src="/resources/img/sandwich/sandwich_cl06.jpg"></label> \
+	<input type="radio" class="hide" id="에그마요" name="main" value="14"> \
+	<label for="이탈리안비엠티"><img alt="이탈리안비엠티" src="/resources/img/sandwich/sandwich_cl01.jpg"></label> \
+	<input type="radio" class="hide" id="이탈리안비엠티" name="main" value="15"> \
+	<label for="미트볼"><img alt="미트볼" src="/resources/img/sandwich/sandwich_cl03.jpg"></label> \
+	<input type="radio" class="hide" id="미트볼" name="main" value="16"> \
+	<label for="햄"><img alt="햄" src="/resources/img/sandwich/sandwich_cl04.jpg"></label> \
+	<input type="radio" class="hide" id="햄" name="main" value="17"> \
+	<label for="참치"><img alt="참치" src="/resources/img/sandwich/sandwich_cl05.jpg"></label> \
+	<input type="radio" class="hide" id="참치" name="main" value="18"> \
+	<label for="사브웨이멜트"><img alt="사브웨이멜트" src="/resources/img/sandwich/sandwich_pm04.jpg"></label> \
+	<input type="radio" class="hide" id="사브웨이멜트" name="main" value="19"> \
+	<label for="로스트치킨"><img alt="로스트치킨" src="/resources/img/sandwich/sandwich_fl02.jpg"></label> \
+	<input type="radio" class="hide" id="로스트치킨" name="main" value="20"> \
+	<label for="비엘티"><img alt="비엘티" src="/resources/img/sandwich/sandwich_cl02.jpg"></label> \
+	<input type="radio" class="hide" id="비엘티" name="main" value="21"> \
+	<label for="터키"><img alt="터키" src="/resources/img/sandwich/sandwich_fl05.jpg"></label> \
+	<input type="radio" class="hide" id="터키" name="main" value="22"> \
 	<h3>빵</h3> \
 	<span>15cm</span><input type="radio" name="is15" value="1" required><span>30cm</span><input type="radio" name="is15" value="0" required><br> \
 	<label for="허니오트"><img alt="허니오트" src="/resources/img/recipe/img_recipe_b01.jpg"></label> \
-	<input type="radio" class="hide" id="허니오트" name="bread" value="0"> \
+	<input type="radio" class="hide" id="허니오트" name="bread" value="1"> \
 	<label for="하티"><img alt="하티" src="/resources/img/recipe/img_recipe_b02.jpg"></label> \
-	<input type="radio" class="hide" id="하티" name="bread" value="1"> \
+	<input type="radio" class="hide" id="하티" name="bread" value="2"> \
 	<label for="위트"><img alt="위트" src="/resources/img/recipe/img_recipe_b03.jpg"></label> \
-	<input type="radio" class="hide" id="위트" name="bread" value="2"> \
+	<input type="radio" class="hide" id="위트" name="bread" value="3"> \
 	<label for="파마산오레가노"><img alt="파마산오레가노" src="/resources/img/recipe/img_recipe_b04.jpg"></label> \
-	<input type="radio" class="hide" id="파마산오레가노" name="bread" value="3"> \
+	<input type="radio" class="hide" id="파마산오레가노" name="bread" value="4"> \
 	<label for="화이트"><img alt="화이트" src="/resources/img/recipe/img_recipe_b05.jpg"></label> \
-	<input type="radio" class="hide" id="화이트" name="bread" value="4"> \
+	<input type="radio" class="hide" id="화이트" name="bread" value="5"> \
 	<label for="플랫브레드"><img alt="플랫브레드" src="/resources/img/recipe/img_recipe_b06.jpg"></label> \
-	<input type="radio" class="hide" id="플랫브래드" name="bread" value="5">';
+	<input type="radio" class="hide" id="플랫브래드" name="bread" value="6">';
 tagSalad = '<h3>메인재료</h3> \
 	<input type="hidden" name=is15 value="-1"><input type="hidden" name="bread" value="-1"> \
-	<label for="이탈리안비엠티"><img alt="이탈리안비엠티" src="/resources/img/salad/salad_cl01.jpg"></label> \
-	<input type="radio" class="hide" id="이탈리안비엠티" name="main" value="0"> \
-	<label for="비엘티"><img alt="비엘티" src="/resources/img/salad/salad_cl02.jpg"></label> \
-	<input type="radio" class="hide" id="비엘티" name="main" value="1"> \
-	<label for="미트볼"><img alt="미트볼" src="/resources/img/salad/salad_cl03.jpg"></label> \
-	<input type="radio" class="hide" id="미트볼" name="main" value="2"> \
-	<label for="햄"><img alt="햄" src="/resources/img/salad/salad_cl04.jpg"></label> \
-	<input type="radio" class="hide" id="햄" name="main" value="3"> \
-	<label for="참치"><img alt="참치" src="/resources/img/salad/salad_cl05.jpg"></label> \
-	<input type="radio" class="hide" id="참치" name="main" value="4"> \
-	<label for="에그마요"><img alt="에그마요" src="/resources/img/salad/salad_cl06.jpg"></label> \
-	<input type="radio" class="hide" id="에그마요" name="main" value="5"> \
-	<label for="로티세리치킨"><img alt="로티세리" src="/resources/img/salad/salad_fl01.jpg"></label> \
-	<input type="radio" class="hide" id="로티세리치킨" name="main" value="6"> \
-	<label for="로스트치킨"><img alt="로스트치킨" src="/resources/img/salad/salad_fl02.jpg"></label> \
-	<input type="radio" class="hide" id="로스트치킨" name="main" value="7"> \
-	<label for="로스트비프"><img alt="로스트비프" src="/resources/img/salad/salad_fl03.jpg"></label> \
-	<input type="radio" class="hide" id="로스트비프" name="main" value="8"> \
-	<label for="사브웨이클럽"><img alt="사브웨이클럽" src="/resources/img/salad/salad_fl04.jpg"></label> \
-	<input type="radio" class="hide" id="사브웨이클럽" name="main" value="9"> \
-	<label for="터키"><img alt="터키" src="/resources/img/salad/salad_fl05.jpg"></label> \
-	<input type="radio" class="hide" id="터키" name="main" value="10"> \
-	<label for="베지"><img alt="베지" src="/resources/img/salad/salad_fl06.jpg"></label> \
-	<input type="radio" class="hide" id="베지" name="main" value="11"> \
-	<label for="스테이크&치즈"><img alt="스테이크&치즈" src="/resources/img/salad/salad_pm01.jpg"></label> \
-	<input type="radio" class="hide" id="스테이크&치즈" name="main" value="12"> \
-	<label for="터키베이컨아보카도"><img alt="터키베이컨아보카도" src="/resources/img/salad/salad_pm02.jpg"></label> \
-	<input type="radio" class="hide" id="터키베이컨아보카도" name="main" value="13"> \
-	<label for="치킨베이컨랜치"><img alt="치킨베이컨랜치" src="/resources/img/salad/salad_pm03.jpg"></label> \
-	<input type="radio" class="hide" id="치킨베이컨랜치" name="main" value="14"> \
-	<label for="사브웨이멜트"><img alt="사브웨이멜트" src="/resources/img/salad/salad_pm04.jpg"></label> \
-	<input type="radio" class="hide" id="사브웨이멜트" name="main" value="15"> \
-	<label for="터키베이컨"><img alt="터키베이컨" src="/resources/img/salad/salad_pm05.jpg"></label> \
-	<input type="radio" class="hide" id="터키베이컨" name="main" value="16"> \
-	<label for="스파이시이탈리안"><img alt="스파이시이탈리안" src="/resources/img/salad/salad_pm06.jpg"></label> \
-	<input type="radio" class="hide" id="스파이시이탈리안" name="main" value="17"> \
-	<label for="치킨데리야끼"><img alt="치킨데리야끼" src="/resources/img/salad/salad_pm07.jpg"></label> \
-	<input type="radio" class="hide" id="치킨데리야끼" name="main" value="18"> \
-	<label for="풀드포크"><img alt="풀드포크" src="/resources/img/salad/salad_pm08.jpg"></label> \
-	<input type="radio" class="hide" id="풀드포크" name="main" value="19"> \
 	<label for="쉬림프"><img alt="쉬림프" src="/resources/img/salad/salad_pm10.jpg"></label> \
-	<input type="radio" class="hide" id="쉬림프" name="main" value="20"> \
+	<input type="radio" class="hide" id="쉬림프" name="main" value="1"> \
 	<label for="쉬림프아보카도"><img alt="쉬림프아보카도" src="/resources/img/salad/salad_pm11.jpg"></label> \
-	<input type="radio" class="hide" id="쉬림프아보카도" name="main" value="21">';
+	<input type="radio" class="hide" id="쉬림프아보카도" name="main" value="2"> \
+	<label for="풀드포크"><img alt="풀드포크" src="/resources/img/salad/salad_pm08.jpg"></label> \
+	<input type="radio" class="hide" id="풀드포크" name="main" value="3"> \
+	<label for="스테이크&치즈"><img alt="스테이크&치즈" src="/resources/img/salad/salad_pm01.jpg"></label> \
+	<input type="radio" class="hide" id="스테이크&치즈" name="main" value="4"> \
+	<label for="터키베이컨아보카도"><img alt="터키베이컨아보카도" src="/resources/img/salad/salad_pm02.jpg"></label> \
+	<input type="radio" class="hide" id="터키베이컨아보카도" name="main" value="5"> \
+	<label for="치킨베이컨랜치"><img alt="치킨베이컨랜치" src="/resources/img/salad/salad_pm03.jpg"></label> \
+	<input type="radio" class="hide" id="치킨베이컨랜치" name="main" value="6"> \
+	<label for="터키베이컨"><img alt="터키베이컨" src="/resources/img/salad/salad_pm05.jpg"></label> \
+	<input type="radio" class="hide" id="터키베이컨" name="main" value="7"> \
+	<label for="스파이시이탈리안"><img alt="스파이시이탈리안" src="/resources/img/salad/salad_pm06.jpg"></label> \
+	<input type="radio" class="hide" id="스파이시이탈리안" name="main" value="8"> \
+	<label for="치킨데리야끼"><img alt="치킨데리야끼" src="/resources/img/salad/salad_pm07.jpg"></label> \
+	<input type="radio" class="hide" id="치킨데리야끼" name="main" value="9"> \
+	<label for="로티세리치킨"><img alt="로티세리" src="/resources/img/salad/salad_fl01.jpg"></label> \
+	<input type="radio" class="hide" id="로티세리치킨" name="main" value="10"> \
+	<label for="로스트비프"><img alt="로스트비프" src="/resources/img/salad/salad_fl03.jpg"></label> \
+	<input type="radio" class="hide" id="로스트비프" name="main" value="11"> \
+	<label for="사브웨이클럽"><img alt="사브웨이클럽" src="/resources/img/salad/salad_fl04.jpg"></label> \
+	<input type="radio" class="hide" id="사브웨이클럽" name="main" value="12"> \
+	<label for="베지"><img alt="베지" src="/resources/img/salad/salad_fl06.jpg"></label> \
+	<input type="radio" class="hide" id="베지" name="main" value="13"> \
+	<label for="에그마요"><img alt="에그마요" src="/resources/img/salad/salad_cl06.jpg"></label> \
+	<input type="radio" class="hide" id="에그마요" name="main" value="14"> \
+	<label for="이탈리안비엠티"><img alt="이탈리안비엠티" src="/resources/img/salad/salad_cl01.jpg"></label> \
+	<input type="radio" class="hide" id="이탈리안비엠티" name="main" value="15"> \
+	<label for="미트볼"><img alt="미트볼" src="/resources/img/salad/salad_cl03.jpg"></label> \
+	<input type="radio" class="hide" id="미트볼" name="main" value="16"> \
+	<label for="햄"><img alt="햄" src="/resources/img/salad/salad_cl04.jpg"></label> \
+	<input type="radio" class="hide" id="햄" name="main" value="17"> \
+	<label for="참치"><img alt="참치" src="/resources/img/salad/salad_cl05.jpg"></label> \
+	<input type="radio" class="hide" id="참치" name="main" value="18"> \
+	<label for="사브웨이멜트"><img alt="사브웨이멜트" src="/resources/img/salad/salad_pm04.jpg"></label> \
+	<input type="radio" class="hide" id="사브웨이멜트" name="main" value="19"> \
+	<label for="로스트치킨"><img alt="로스트치킨" src="/resources/img/salad/salad_fl02.jpg"></label> \
+	<input type="radio" class="hide" id="로스트치킨" name="main" value="20"> \
+	<label for="비엘티"><img alt="비엘티" src="/resources/img/salad/salad_cl02.jpg"></label> \
+	<input type="radio" class="hide" id="비엘티" name="main" value="21"> \
+	<label for="터키"><img alt="터키" src="/resources/img/salad/salad_fl05.jpg"></label> \
+	<input type="radio" class="hide" id="터키" name="main" value="22">';
