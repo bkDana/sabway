a2b3acdd3d37b9c748e8de2e77c7db7ea503dc6b
조성일 <roseshade90@gmail.com>
2019-07-15 11:45:01 +0900

    프로모션 추가(아직 다중 x)

--- src/main/java/kr/co/subway/headOffice/dao/MenuDAO.java
+++ src/main/java/kr/co/subway/headOffice/dao/MenuDAO.java
@@ -7,17 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.co.subway.headOffice.vo.Menu;
+import kr.co.subway.ingreManage.vo.IngreVo;
 
 @Repository("menuDao")
 public class MenuDAO {
 	@Autowired
 	private SqlSession session;
 	//메뉴목록
-	public List<Menu> menuList() {
-		return session.selectList("menu.menuList");
+	public List<IngreVo> menuList() {
+//		return session.selectList("menu.menuList");
+		return session.selectList("ingre.ingreMainList");
 	}
 	//할인 및 할인율 수정
-	public int updateMenu(Menu menu) {
-		return session.update("menu.updateMenu",menu);
+	public int updateIngre(IngreVo ingre) {
+		System.out.println("dao : "+ingre.getIngreDiscntRate());
+		System.out.println("dao : "+ingre.getIngreLabel());
+		System.out.println("dao : "+ingre.getIngreType());
+		return session.update("ingre.updateIngre",ingre);
 	}
+	//메뉴목록
+//	public List<Menu> menuList() {
+//		return session.selectList("menu.menuList");
+//	}
+//	//할인 및 할인율 수정
+//	public int updateMenu(Menu menu) {
+//		return session.update("menu.updateMenu",menu);
+//	}
 }
