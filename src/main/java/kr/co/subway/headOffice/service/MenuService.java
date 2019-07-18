e6ce9ee118f226573f72011e22091e90999a206d
조성일 <roseshade90@gmail.com>
2019-07-18 17:57:26 +0900

    프로모션 상품명 검색, 페이징 추가

--- src/main/java/kr/co/subway/headOffice/service/MenuService.java
+++ src/main/java/kr/co/subway/headOffice/service/MenuService.java
@@ -13,6 +13,7 @@ import kr.co.subway.customerOrder.vo.CusOrderPageBound;
 import kr.co.subway.customerOrder.vo.CusOrderPageData;
 import kr.co.subway.headOffice.dao.MenuDAO;
 import kr.co.subway.headOffice.vo.ApplyMenuPageBound;
+import kr.co.subway.headOffice.vo.ApplyMenuPageData;
 import kr.co.subway.headOffice.vo.ApplyPageBound;
 import kr.co.subway.headOffice.vo.ApplyPageData;
 import kr.co.subway.headOffice.vo.Menu;
@@ -34,32 +35,62 @@ public class MenuService {
 		return result;
 	}
 	//메뉴목록 페이징
-	public ApplyPageData menuListPaging(int currentPage1){
+	public ApplyMenuPageData menuListPaging(int currentPage1){
 		String pageNavi = "";
 		int numPerPage = 10;
 		int pageNaviSize = 5;
-		int totalCount = menudao.menuListTotalCount();
+		String keyword = "메인재료";
+		int totalCount = menudao.menuListTotalCount(keyword);
 		int totalPage = (totalCount%numPerPage==0)?(totalCount/numPerPage):(totalCount/numPerPage)+1;
 		int start = (currentPage1-1)*numPerPage+1;
 		int end = currentPage1*numPerPage;
-		ApplyMenuPageBound pb = new ApplyMenuPageBound(start,end,null);
+		ApplyMenuPageBound pb = new ApplyMenuPageBound(start,end,keyword);
 		ArrayList<IngreVo> list = (ArrayList<IngreVo>) menudao.menuListPaging(pb);
 		int pageNo = ((currentPage1-1)/pageNaviSize)*pageNaviSize+1;
 		if(currentPage1 != 1) {
-			pageNavi += "<a href='/cusOrderList.do?currentPage="+(currentPage1-1)+"'>이전</a>&nbsp;";
+			pageNavi += "<a href='/promotionSelect.do?currentPage="+(currentPage1-1)+"'>이전</a>&nbsp;";
 		}
 		int i = 1;
 		while(!(i++>pageNaviSize||pageNo>totalPage)) {
 			if(currentPage1 == pageNo) {
 				pageNavi += "&nbsp;<span style='color:black;font-size:20px;'>"+pageNo+"</span>&nbsp;";
 			}else {
-				pageNavi += "&nbsp;<a href='/cusOrderList.do?currentPage="+pageNo+"'>"+pageNo+"<a/>&nbsp;";
+				pageNavi += "&nbsp;<a href='/promotionSelect.do?currentPage="+pageNo+"'>"+pageNo+"<a/>&nbsp;";
 			}
 			pageNo++;
 		}
 		if(currentPage1 < totalPage) {
-			pageNavi += "&nbsp;<a href='/cusOrderList.do?currentPage="+(currentPage1+1)+"'>다음</a>";
+			pageNavi += "&nbsp;<a href='/promotionSelect.do?currentPage="+(currentPage1+1)+"'>다음</a>";
 		}
-		return new CusOrderPageData(list,pageNavi,null);
+		return new ApplyMenuPageData(list,pageNavi,totalCount);
+	}
+	//검색목록 페이징
+	public ApplyMenuPageData searchPromotion(int currentPage1,String keyword){
+		String pageNavi = "";
+		int numPerPage = 10;
+		int pageNaviSize = 5;
+		int totalCount = menudao.menuListTotalCount(keyword);
+		int totalPage = (totalCount%numPerPage==0)?(totalCount/numPerPage):(totalCount/numPerPage)+1;
+		int start = (currentPage1-1)*numPerPage+1;
+		int end = currentPage1*numPerPage;
+		ApplyMenuPageBound pb = new ApplyMenuPageBound(start,end,keyword);
+		ArrayList<IngreVo> list = (ArrayList<IngreVo>) menudao.menuListPaging(pb);
+		int pageNo = ((currentPage1-1)/pageNaviSize)*pageNaviSize+1;
+		if(currentPage1 != 1) {
+			pageNavi += "<a href='/promotionSelect.do?currentPage="+(currentPage1-1)+"'>이전</a>&nbsp;";
+		}
+		int i = 1;
+		while(!(i++>pageNaviSize||pageNo>totalPage)) {
+			if(currentPage1 == pageNo) {
+				pageNavi += "&nbsp;<span style='color:black;font-size:20px;'>"+pageNo+"</span>&nbsp;";
+			}else {
+				pageNavi += "&nbsp;<a href='/promotionSelect.do?currentPage="+pageNo+"'>"+pageNo+"<a/>&nbsp;";
+			}
+			pageNo++;
+		}
+		if(currentPage1 < totalPage) {
+			pageNavi += "&nbsp;<a href='/promotionSelect.do?currentPage="+(currentPage1+1)+"'>다음</a>";
+		}
+		return new ApplyMenuPageData(list,pageNavi,totalCount);
 	}
 }
