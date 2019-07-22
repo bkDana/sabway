525c505c1c5a6d83c6fac6f0046fa056488d675b
emthrology <emthrology@gmail.com>
2019-07-22 13:52:53 +0900

    테스트용 주석

--- src/main/webapp/resources/js/bucket.js
+++ src/main/webapp/resources/js/bucket.js
@@ -480,49 +480,58 @@ $(document).ready(function(){
     /* 아임포트 */
     
     $('#sbmOrder').click(function(){
-    	console.log(totalCost);
-		var d = new Date();
+    	//테스트용으로 임시 비활성화
+//    	console.log(totalCost);
+//		var d = new Date();
+//		var date = d.getFullYear()+''+(d.getMonth()+1)+''+d.getDate()+''+d.getHours()+''+d.getMinutes()+''+d.getSeconds();
+//		IMP.init('imp25889583');
+//		IMP.request_pay({
+//			pay_method : 'card',
+//			merchant_uid : $('.hiddenInfo').eq(0).find('.hiddenMain').val()+$('.hiddenInfo').eq(0).find('.hiddenIsSalad').val()+date,				//거래ID - 유니크 주려고 날짜까지 넣음
+//			name : $('.hiddenInfo').eq(0).find('.hiddenMain').val()+" 외",						//결재명
+//			buyer_name : sessionId,
+//			buyer_email : '',
+//			amount : totalCost,									//결재 금액
+//			buyer_tel : sessionPhone
+//			
+//		},function(response){
+//			if(response.success){
+//				var msg = "결재가 완료되었습니다.";
+//				var info1 = "고유 ID : "+response.imp_uid;
+//				var info2 = "결재 금액 : "+response.paid_amount;
+//				var info3 = "카드 승인 번호 : "+response.apply_num;
+//				console.log(msg+"<br>"+info1+"<br>"+info2+"<br>"+info3);
+//				$('input[name=cusoCallBy').val(sessionId);
+//				$('input[name=cusoTotalCost]').val(totalCost);
+//		    	$('input[name=cusoPhone]').val(sessionPhone);
+//		    	var d = new Date();
+//				var date = d.getFullYear()+''+(d.getMonth()+1)+''+d.getDate()+''+d.getHours()+''+d.getMinutes()+''+d.getSeconds();
+//		    	$('input[name=cusoOrderNo]').val(date);
+//
+//				$("#insertOrder").click();
+//			} else {
+//				alert('결재가 취소되었습니다');
+//			}
+//		});
+		$('input[name=cusoCallBy').val(sessionId);
+		$('input[name=cusoTotalCost]').val(totalCost);
+    	$('input[name=cusoPhone]').val(sessionPhone);
+    	var d = new Date();
 		var date = d.getFullYear()+''+(d.getMonth()+1)+''+d.getDate()+''+d.getHours()+''+d.getMinutes()+''+d.getSeconds();
-		IMP.init('imp25889583');
-		IMP.request_pay({
-			pay_method : 'card',
-			merchant_uid : $('.hiddenInfo').eq(0).find('.hiddenMain').val()+$('.hiddenInfo').eq(0).find('.hiddenIsSalad').val()+date,				//거래ID - 유니크 주려고 날짜까지 넣음
-			name : $('.hiddenInfo').eq(0).find('.hiddenMain').val()+" 외",						//결재명
-			buyer_name : sessionId,
-			buyer_email : '',
-			amount : totalCost,									//결재 금액
-			buyer_tel : sessionPhone
-			
-		},function(response){
-			if(response.success){
-				var msg = "결재가 완료되었습니다.";
-				var info1 = "고유 ID : "+response.imp_uid;
-				var info2 = "결재 금액 : "+response.paid_amount;
-				var info3 = "카드 승인 번호 : "+response.apply_num;
-				console.log(msg+"<br>"+info1+"<br>"+info2+"<br>"+info3);
-				$('input[name=cusoCallBy').val(sessionId);
-				$('input[name=cusoTotalCost]').val(totalCost);
-		    	$('input[name=cusoPhone]').val(sessionPhone);
-		    	var d = new Date();
-				var date = d.getFullYear()+''+(d.getMonth()+1)+''+d.getDate()+''+d.getHours()+''+d.getMinutes()+''+d.getSeconds();
-		    	$('input[name=cusoOrderNo]').val(date);
+    	$('input[name=cusoOrderNo]').val(date);
 
-				$("#insertOrder").click();
-			} else {
-				alert('결재가 취소되었습니다');
-			}
-		});
+		$("#insertOrder").click();
 	});
     
 
     /* 나만의메뉴 팝업 */
-    function popup() {
-    	window.name="searchParent";
-		var url = "loadSearchStorePopup.do";
-		var name = "searchChild";
-		var option="width=300, height=500, top=200, left=350, location=no"
-		openWin = window.open(url, name, option);
-	}
+//    function popup() {
+//    	window.name="searchParent";
+//		var url = "loadSearchStorePopup.do";
+//		var name = "searchChild";
+//		var option="width=300px, height=500px, top=0px, left=350px, location=no"
+//		openWin = window.open(url, name, option);
+//	}
     
     $('.orderMyMenu').click(function(){
     	var i = $('.orderMyMenu').index(this);
@@ -542,8 +551,10 @@ $(document).ready(function(){
     	$('.kcal').val($('.hiddenKcal').eq(i).val());
     	$('.quantity').val($('.hiddenQuantity').eq(i).val());
 
-    	popup();
-    	var option="width=600, height=500, top=50, left=650, location=no"
+    	//popup();
+    	var popupX = (window.screen.width / 2) - (200 / 2);
+    	var popupY= (window.screen.height / 2) - (300 / 2);
+    	var option="width=300px, height=500px, top=-100px, left='+ popupX + ', top='+ popupY"
     	cldwin = window.open("loadSearchStorePopup.do","searchChild",option);	
     	
     	cldwin.focus();
